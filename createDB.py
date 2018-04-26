87d0724f3b96b10fcf51c5a318510b6676648aec
shahar <shahar.aizenbud@gmail.com>
2018-04-26 13:16:45 +0300

    server

--- createDB.py
+++ createDB.py
@@ -2,9 +2,13 @@ import sqlite3
 
 conn = sqlite3.connect('medicine_db.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE meds
-             (uid integer, med_name text, expiration_data text, amount real,
-              is_closed integer, city text, owner_mail test)''')
+# c.execute('''CREATE TABLE meds
+#              (uid integer, med_name text, expiration_data text, amount real,
+#               is_closed integer, city text, owner_mail test)''')
+
+c.execute("INSERT INTO meds VALUES (1, 'Malarone', '2018-05-01', 10, 0, 'Haifa', 'shahar.aizenbud@gmail.com')")
+
+
 conn.commit()
 
 conn.close()
--- server.py
+++ server.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+import sqlite3
+import json
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "Hello World"
+@app.route("/search")
+def search():
+    return render_template()
+
+
+@app.route("/query", methods=['POST'])
+def query():
+    conn = sqlite3.connect('medicine_db.db')
+    c = conn.cursor()
+
+    c.execute('select * from meds')
 
 
 if __name__ == "__main__":
