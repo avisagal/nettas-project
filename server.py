8e6819adbaba06094d426c4c119dab5ed52cfee8
shahar <shahar.aizenbud@gmail.com>
2018-04-26 18:12:47 +0300

    html

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ import json
 import mich
 
 app = Flask(__name__)
-cur_uid = 5
 
 @app.route("/")
 def index():
@@ -16,42 +15,42 @@ def index():
 
 @app.route("/check=<name>")
 def check(name):
-    conn = sqlite3.connect('medicine_db.db')
-    c = conn.cursor()
     name = mich.my_trans(name)
-    c.execute('''select distinct med_name from meds_data''')
-    all_meds = []
-    for med in c.fetchall():
-        all_meds.append(med[0])
     name = mich.best_word(all_meds, name)
     name = (name,)
     c.execute('''select meds.uid, meds.med_name, meds.amount, meds.city,
-                  meds.expiration_data, meds.is_closed, meds_data.picture
+                  meds.expiration_date, meds.is_closed, meds.owner_name, meds_data.picture
                   from meds
                   inner join meds_data on meds.med_name = meds_data.med_name
                   where meds.med_name = ? collate nocase''', name)
     json_return = json.dumps(c.fetchall())
-    conn.close()
 
     return jsonify(json_return)
 
+
 @app.route("/add", methods=['POST'])
 def add():
+    global uid
     data = request.args
-    uid = int(data["uid"])
-    med_name = data["med_name"]
-    expiration_date = data["date"]  # how do we get it?
-    amount = float(data["amount"])
-    is_closed = data["is_closed"]  # how do we get it?
-    city = data["city"]
-    owner_mail = data["mail"]
-    name = data["name"]
-
-    med_name = mich.
-
-
-
+    med_name = mich.best_word(all_meds, data["med_name"])
+    if med_name is None:
+        return jsonify(json.dumps({'state': 1}))
+    details = (uid, med_name, data["date"], float(data["amount"]), data["is_closed"],
+               data["city"], data["mail"], data["name"])
+    uid += 1
+    c.execute("INSERT INTO meds VALUES (?,?,?,?,?,?,?,?)", details)
+    conn.commit()
+    return jsonify(json.dumps({'state': 0}))
 
 
 if __name__ == "__main__":
+    conn = sqlite3.connect('medicine_db.db')
+    c = conn.cursor()
+    c.execute('''select distinct med_name from meds_data''')
+    all_meds = []
+    for med in c.fetchall():
+        all_meds.append(med[0])
+    c.execute('''select max(uid) from meds''')
+    uid = c.fetchone()[0] + 1
     app.run()
+    conn.close()
