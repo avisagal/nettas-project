97e4200323eb71dfa72c3aa1f6aedc3c82d7622d
shahar <shahar.aizenbud@gmail.com>
2018-04-26 12:39:13 +0300

    DB examples

--- createDB.py
+++ createDB.py
@@ -2,6 +2,9 @@ import sqlite3
 
 conn = sqlite3.connect('medicine_db.db')
 c = conn.cursor()
-
+c.execute('''CREATE TABLE meds
+             (uid integer, med_name text, expiration_data text, amount real,
+              is_closed integer, city text, owner_mail test)''')
+conn.commit()
 
 conn.close()
