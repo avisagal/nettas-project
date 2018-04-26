1509e2723a15e9ae378a47606fd5c55b2c29f572
shahar <shahar.aizenbud@gmail.com>
2018-04-26 13:28:28 +0300

    server

--- createDB.py
+++ createDB.py
@@ -6,7 +6,12 @@ c = conn.cursor()
 #              (uid integer, med_name text, expiration_data text, amount real,
 #               is_closed integer, city text, owner_mail test)''')
 
-c.execute("INSERT INTO meds VALUES (1, 'Malarone', '2018-05-01', 10, 0, 'Haifa', 'shahar.aizenbud@gmail.com')")
+#c.execute("INSERT INTO meds VALUES (1, 'Malarone', '2018-05-01', 10, 0, 'Haifa', 'shahar.aizenbud@gmail.com')")
+
+values = [(2, 'Acamol', '2018-06-22', 20, 1, 'Jerusalem', 'avisagal41@gmail.com'),
+          (3, 'Advil', '2019-01-01', 15, 0, 'Jerusalem', 'netta.barak@gmail.com')]
+c.executemany("INSERT INTO meds VALUES (?,?,?,?,?,?,?)", values)
+
 
 
 conn.commit()
